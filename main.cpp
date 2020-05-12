6a07d2ae55d4fa892f20093c6f3dadc1b9f6fd79
Braulio <braulio.argandonac@utem.cl>
2020-05-11 21:44:14 -0400

    Declaracion de Clases

--- main.cpp
+++ main.cpp
@@ -96,3 +96,30 @@ double promediar(std::vector<int> puntajes) {
     }
     return promedio;
 }
+
+class Estudiante{
+    private:
+        int rut;
+        double promedio;
+    public:
+        double GetPromedio;
+        int GetRut;
+        void SetPromedio(double y);
+        void SetRut(int x);
+}
+
+double Estudiante::GetPromedio(){
+    return promedio;
+}
+
+int Estudiante::GetRut(){
+    return rut;
+}
+
+void Estudiante::SetPromedio(double y){
+    promedio=y;
+}
+
+void Estudiante::SetRut(int x){
+    rut=x;
+}
